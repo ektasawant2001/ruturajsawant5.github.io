69f0f527bfdfb7abaa3b0d51e00a8e376d608171
ruturajsawant5 <ruturajsawant5@gmail.com>
2019-07-31 22:09:58 +0530

    v6

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-var sections = $('section')
-  , nav = $('nav')
-  , nav_height = nav.outerHeight();
+// Add scrollspy to <body>
+$('body').scrollspy({target: ".navbar", offset: 50});
 
-$(window).on('scroll', function () {
-  var cur_pos = $(this).scrollTop();
+// Add smooth scrolling on all links inside the navbar
+$("#myNavbar a").on('click', function(event) {
 
-  sections.each(function() {
-    var top = $(this).offset().top - nav_height,
-        bottom = top + $(this).outerHeight();
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
 
-    if (cur_pos >= top && cur_pos <= bottom) {
-      nav.find('a').removeClass('active');
-      sections.removeClass('active');
+    // Prevent default anchor click behavior
+    event.preventDefault();
 
-      $(this).addClass('active');
-      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-    }
-  });
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+
+    // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+
+  } // End if
+
 });
